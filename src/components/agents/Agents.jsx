36822348c759c6ba34bfe0b8bb3ba60a8ed940f2
Rohan Nayan <rohannayan405@gmail.com>
2023-06-18 02:25:53 +0530

    Added Agents Component

--- src/components/agents/Agents.jsx
+++ src/components/agents/Agents.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import './agents.css';
+import Data from './agentData';
 
-function Agents(){
-    return(
-        <div>Agents</div>
+function Agents() {
+    return (
+        <div className="agents section_padding" id="agents">
+            <div className="agents-heading">
+                <h1>Agents</h1>
+            </div>
+            <div className="agents-container">
+                {Data.map(data => {
+                    return (
+                        <div className="agents-preview">
+                            <img src={data.img} alt="Agent" />
+                            <h4>{data.name}</h4>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/agents/agentData.js
@@ -0,0 +1,46 @@
+import image from '../../assets/jett.png'
+
+const Data = [
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    },
+    {
+        name: "Jett",
+        img: image
+    }
+];
+  
+export default Data;
