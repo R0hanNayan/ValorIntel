1fc7a1c1d6580e805c324e62d5ab81a7f21b3d2a
Rohan Nayan <rohannayan405@gmail.com>
2023-06-18 00:13:47 +0530

    Added Complete Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,23 +6,24 @@ import './header.css';
 
 function Header() {
     return (
-        <div className="Header" id="home">
+        <div className="header section_padding" id="home">
             <div className="header-container-content">
                 <h1>Valorant</h1>
                 <h4>A 5v5 character-based tactical shooter</h4>
                 <h2>ValorIntel</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div className="header-btn">
+                    <a style={{ textDecoration: "none" }} href="#agents">Get Started</a>
+                </div>
             </div>
-            <div className="header-btn">
-                <a style={{ textDecoration: "none", color: "inherit" }} href="#agents"><buton type="button">Get Started</buton></a>
-            </div>
+
             <div className="header-model">
-                <Canvas>
+                <Canvas className="canvas">
                     <OrbitControls enableZoom={false} />
                     <ambientLight intensity={0.5} />
-                    <directionalLight position={[-2, 5, 2]} intensity={1} />
+                    <directionalLight position={[-2, 5, 2]} intensity={1} castShadow/>
                     <Suspense fallback={null}>
-                        <Raze />
+                        <Raze rotation={[0.1, -0.6, 0]} position={[0, -1.2, 0]} />
                     </Suspense>
                 </Canvas>
             </div>
