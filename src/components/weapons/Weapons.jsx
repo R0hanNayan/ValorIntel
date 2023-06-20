9f3adfd43f66395ab39c59b6bc7dd251bba6c335
Rohan Nayan <rohannayan405@gmail.com>
2023-06-20 23:43:32 +0530

    Added Weapons Component

--- src/components/weapons/Weapons.jsx
+++ src/components/weapons/Weapons.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Data from './weaponData';
+import './weapons.css';
 
-function Weapons(){
-    return(
-        <div>Weapons</div>
-    );
+function Weapons() {
+    return (
+        <div className="weapons section_padding">
+            <div className="weapons-heading">
+                <h1>Weapons</h1>
+            </div>
+            <div className="weapons-container">
+                {Data.map((data, index) => {
+                    return (
+                        <div>
+                            <div className="weapons-preview">
+                                <img src={data.img} alt="weapon" />
+                                <h4>{data.name}</h4>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Weapons;
---
+++ src/components/weapons/weaponData.js
@@ -0,0 +1,20 @@
+const Data = [
+    {
+        name: "Vandal",
+        img: require("../../assets/vandal/vandal.png")
+    },
+    {
+        name: "Vandal",
+        img: require("../../assets/vandal/vandal.png")
+    },
+    {
+        name: "Vandal",
+        img: require("../../assets/vandal/vandal.png")
+    },
+    {
+        name: "Vandal",
+        img: require("../../assets/vandal/vandal.png")
+    }
+];
+
+export default Data;
